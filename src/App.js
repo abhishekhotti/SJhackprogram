d508f0fe45b470b3896f28bba6d5bbbede099b36
abhishekhotti <abhishek.hotti@sjsu.edu>
2019-04-28 00:20:42 -0700

    giles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from "react-bootstrap/Button";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{ background: "black" }}>
+      <Navbar variant="dark">
+        <Container>
+          <Navbar.Brand style={{ margin: "auto" }} href="./">
+            Home
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <Button
+          style={{ padding: "30px", width: "75%" }}
+          size="lg"
+          block="true"
+          type="button"
+          variant="outline-info"
+          href="./housing"
         >
-          Learn React
-        </a>
+          Housing
+        </Button>
+        <Button
+          style={{ padding: "30px", width: "75%", marginTop: "25px" }}
+          size="lg"
+          block="true"
+          type="button"
+          variant="outline-info"
+          href="./jobs"
+        >
+          Jobs
+        </Button>
+        <Button
+          style={{ padding: "30px", width: "75%", marginTop: "25px" }}
+          size="lg"
+          block="true"
+          type="button"
+          variant="outline-info"
+          href="./therapy"
+        >
+          Counseling
+        </Button>
       </header>
     </div>
   );
